95f3480b3e3ab5c35e49290bd858d242f5a79234
Stian Hallerud Sundkvist <stian.sundkv@gmail.com>
2025-02-22 19:31:43 +0100

    add missing files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,16 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      keyframes: {
+        meteor: {
+          "0%": { transform: "translateY(-20%) translateX(-50%)" },
+          "100%": { transform: "translateY(300%) translateX(-50%)" },
+        },
+      },
+      animation: {
+        meteor: "meteor var(--duration) var(--delay) ease-in-out infinite",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
